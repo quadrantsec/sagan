e195f1732b9a9ea6b93a9efa753c7e3c8cc6f4eb
root <root@dev.quadrantsec.com>
2021-03-18 01:11:33 +0000

    Fixed segfault on shutdown trying to remove lock.

--- src/signal-handler.c
+++ src/signal-handler.c
@@ -261,7 +261,6 @@ void Sig_Handler( void )
                             Client_Stats_Close();
                         }
 
-                    Remove_Lock_File();
                     exit(0);
                     break;
 
