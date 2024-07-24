962e3adf2d1cda2da07e3c047a09338006d4f9af
Champ Clark - 8504432440 [admin] <champ.clark@dev.quadrantsec.com>
2024-07-24 23:47:51 +0000

    New offload features.

---
+++ src/offload.c
@@ -0,0 +1,136 @@
+/*
+** Copyright (C) 2009-2024 Quadrant Information Security <quadrantsec.com>
+** Copyright (C) 2009-2024 Champ Clark III <cclark@quadrantsec.com>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License Version 2 as
+** published by the Free Software Foundation.  You may not use, modify or
+** distribute this program under any other version of the GNU General
+** Public License.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+/* This handles "offload" rule option */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"             /* From autoconf */
+#endif
+
+#ifdef WITH_OFFLOAD
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <curl/curl.h>
+
+#include "sagan.h"
+#include "sagan-defs.h"
+#include "sagan-config.h"
+#include "rules.h"
+#include "offload.h"
+
+#include "parsers/parsers.h"
+
+extern struct _Rule_Struct *rulestruct;
+extern struct _SaganConfig *config;
+extern struct _SaganDebug *debug;
+
+bool Offload( uint_fast32_t rule_position, const char *syslog_host, const char *syslog_facility, const char *syslog_priority, const char *syslog_level, const char *syslog_tag, const char *syslog_date, const char *syslog_time, const char *syslog_program, const char *syslog_message )
+{
+
+    CURL *curl;
+    char *response=NULL;
+    struct curl_slist *headers = NULL;
+
+    char buf[ MAX_SYSLOG_HOST + MAX_SYSLOG_FACILITY + MAX_SYSLOG_PRIORITY + MAX_SYSLOG_LEVEL + MAX_SYSLOG_TAG + MAX_SYSLOG_DATE + MAX_SYSLOG_TIME + MAX_SYSLOG_PROGRAM + MAX_SYSLOGMSG + 1 ] = { 0 };
+
+
+    /* Build POST data to send */
+
+    snprintf(buf, sizeof(buf), "%s|%s|%s|%s|%s|%s|%s|%s|%s", syslog_host, syslog_facility, syslog_priority, syslog_level, syslog_tag, syslog_date, syslog_time, syslog_program, syslog_message);
+
+    if ( debug->debugoffload == true )
+        {
+            Sagan_Log(WARN, "Sending data to %s for signature id %" PRIuFAST64 "", rulestruct[rule_position].offload_location, rulestruct[rule_position].s_sid);
+            Sagan_Log(WARN, "Data: %s", buf);
+        }
+
+    curl = curl_easy_init();
+
+
+    if (curl)
+        {
+
+            curl_easy_setopt(curl, CURLOPT_URL, rulestruct[rule_position].offload_location );
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback_func);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+            curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);    /* Will send SIGALRM if not set */
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, buf);
+
+            headers = NULL;
+            headers = curl_slist_append (headers, USER_AGENT);
+
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers );
+
+            (void)curl_easy_perform(curl);
+
+        }
+    else
+        {
+
+            Sagan_Log(WARN, "Can't call libcurl!");
+            return(false);
+
+        }
+
+
+    if ( response == NULL )
+        {
+            Sagan_Log(WARN, "[%s, line %d] Offload program returned a empty \"response\".", __FILE__, __LINE__);
+            return(false);
+        }
+
+    Remove_Return(response);
+
+    if ( Sagan_stristr( response, "true", false) )
+        {
+
+            if ( debug->debugoffload == true )
+                {
+                    Sagan_Log(DEBUG, "%s returned \"true\"", rulestruct[rule_position].offload_location);
+                }
+
+            return( true );
+        }
+
+
+    if ( debug->debugoffload == true )
+        {
+            Sagan_Log(DEBUG, "%s returned \"false\"", rulestruct[rule_position].offload_location);
+        }
+
+    return false;
+
+}
+
+/****************************************************************************
+ * write_callback_func() - Callback for data received via libcurl
+ ****************************************************************************/
+
+size_t static write_callback_func(void *buffer, size_t size, size_t nmemb, void *userp)
+{
+    char **response_ptr =  (char**)userp;
+    *response_ptr = strndup(buffer, (size_t)(size *nmemb));     /* Return the string */
+}
+
+
+
+#endif
---
+++ src/offload.h
@@ -0,0 +1,23 @@
+/*
+** Copyright (C) 2009-2024 Quadrant Information Security <quadrantsec.com>
+** Copyright (C) 2009-2024 Champ Clark III <cclark@quadrantsec.com>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License Version 2 as
+** published by the Free Software Foundation.  You may not use, modify or
+** distribute this program under any other version of the GNU General
+** Public License.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+bool Offload( uint_fast32_t rule_position, const char *syslog_host, const char *syslog_facility, const char *syslog_priority, const char *syslog_level, const char *syslog_tag, const char *syslog_date, const char *syslog_time, const char *syslog_program, const char *syslog_message );
+
+size_t static write_callback_func(void *buffer, size_t size, size_t nmemb, void *userp);
