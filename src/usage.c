46b32fba715b9b25ec249b71c650a048acafb9e0
Champ Clark - 8504432440 [admin] <champ.clark@dev.quadrantsec.com>
2024-10-21 17:45:23 +0000

    Typo

--- src/usage.c
+++ src/usage.c
@@ -83,7 +83,7 @@ void Usage(void)
     fprintf(stderr, "-l, --log [file]\tsagan.log location [default: %s].\n", SAGANLOG );
     fprintf(stderr, "-Q, --quiet\t\tRun Sagan in 'quiet' mode (no console output)\n");
     fprintf(stderr, "-t, --threads [#]\tSet number of threads to use (overrides the sagan.yaml).\n");
-    fprintf(stderr, "-b, --batch [#]\t\tSet the size of the batch size (overrides the sagan.yaml).\n");
+    fprintf(stderr, "-b, --batch [#]\t\tSet the batch size for logs (overrides the sagan.yaml).\n");
     fprintf(stderr, "-r, --rules [file]\tUse a specific rule set (overrides the sagan.yaml).\n");
     fprintf(stderr, "-T, --test\t\tTest configuration (sagan.yaml) and rule sets.\n");
     fprintf(stderr, "\n");
